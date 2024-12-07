b93894f0a8a694146b9c245ead8187f6a098d57c
TimCashen1 <136538240+TimCashen1@users.noreply.github.com>
2024-12-06 20:09:52 -0500

    Delete choiceSelect.js

--- choiceSelect.js
+++
@@ -1,7 +0,0 @@
-const el = document.getElementsByClassName("output");
-function voteBob(){
-    el[0].innerHTML = "You picked Bob!";
-}
-function voteMinion(){
-    el[1].innerHTML = "You picked Minion!";
-}
